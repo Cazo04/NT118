c9d17b5f9db16195d5c1a7c6396c19c33f22faa8
danggbao <dangbao290404@gmail.com>
2024-01-02 00:36:20 +0700

    cài đặt class của object lương

---
+++ app/src/main/java/com/example/nt118/Class/Luong.java
@@ -0,0 +1,102 @@
+package com.example.nt118.Class;
+
+import java.util.Date;
+
+public class Luong {
+    private String ky_thanh_toan;
+    private int luong_co_ban;
+    private int so_gio_lam_viec;
+    private int so_gio_lam_viec_ngoai_gio;
+    private int tien_thuong;
+    private int thue_thu_nhap;
+    private int bao_hiem;
+    private int tien_phat;
+    private int tong_thu_nhap;
+    private boolean da_thanh_toan;
+    private String ngay_thanh_toan;
+    public String getNgay_thanh_toan() {
+        return ngay_thanh_toan;
+    }
+
+    public void setNgay_thanh_toan(String ngay_thanh_toan) {
+        this.ngay_thanh_toan = ngay_thanh_toan;
+    }
+    public String getKy_thanh_toan() {
+        return ky_thanh_toan;
+    }
+    public void setKy_thanh_toan(String ky_thanh_toan) {
+        this.ky_thanh_toan = ky_thanh_toan;
+    }
+
+    public int getLuong_co_ban() {
+        return luong_co_ban;
+    }
+
+    public void setLuong_co_ban(int luong_co_ban) {
+        this.luong_co_ban = luong_co_ban;
+    }
+
+    public int getSo_gio_lam_viec_ngoai_gio() {
+        return so_gio_lam_viec_ngoai_gio;
+    }
+
+    public void setSo_gio_lam_viec_ngoai_gio(int so_gio_lam_viec_ngoai_gio) {
+        this.so_gio_lam_viec_ngoai_gio = so_gio_lam_viec_ngoai_gio;
+    }
+
+    public int getSo_gio_lam_viec() {
+        return so_gio_lam_viec;
+    }
+
+    public void setSo_gio_lam_viec(int so_gio_lam_viec) {
+        this.so_gio_lam_viec = so_gio_lam_viec;
+    }
+
+    public int getTien_phat() {
+        return tien_phat;
+    }
+
+    public void setTien_phat(int tien_phat) {
+        this.tien_phat = tien_phat;
+    }
+
+    public int getTien_thuong() {
+        return tien_thuong;
+    }
+
+    public void setTien_thuong(int tien_thuong) {
+        this.tien_thuong = tien_thuong;
+    }
+
+    public int getThue_thu_nhap() {
+        return thue_thu_nhap;
+    }
+
+    public void setThue_thu_nhap(int thue_thu_nhap) {
+        this.thue_thu_nhap = thue_thu_nhap;
+    }
+
+    public void setBao_hiem(int bao_hiem) {
+        this.bao_hiem = bao_hiem;
+    }
+
+    public int getBao_hiem() {
+        return bao_hiem;
+    }
+
+    public int getTong_thu_nhap() {
+        return tong_thu_nhap;
+    }
+
+    public void setTong_thu_nhap(int tong_thu_nhap) {
+        this.tong_thu_nhap = tong_thu_nhap;
+    }
+
+    public boolean isDa_thanh_toan() {
+        return da_thanh_toan;
+    }
+
+    public void setDa_thanh_toan(boolean da_thanh_toan) {
+        this.da_thanh_toan = da_thanh_toan;
+    }
+}
